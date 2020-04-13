f05525179ca4691b555673d34a23c1461a30e077
Michail <pmihail734@gmail.com>
2020-04-13 20:59:50 +0300

    ...

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const indexRouter = require("./routes/index");
 const accountRouter = require("./routes/account");
 
 mongoose.pluralize(null);
-// "mongodb://localhost/myTreasures"
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
